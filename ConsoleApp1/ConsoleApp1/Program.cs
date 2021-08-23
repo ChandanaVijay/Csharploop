ac986ec27a3153e3c552b8345970879e4c4f305f
cvk96 <chandanavk96@gmail.com>
2021-08-23 15:53:17 +0530

    factorial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1, i, n;
+            Console.WriteLine("Enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                fact = fact * i;
+
+            }
+            Console.WriteLine("Factorial is\t" + fact);
+        }
+    }
+}
