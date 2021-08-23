88dff18a99697914a25e9eb2c1ae1b7b3919c2cf
cvk96 <chandanavk96@gmail.com>
2021-08-23 16:08:19 +0530

    fibonacci

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans, n1 = 0, n2 = 1,i,n3;
+            Console.WriteLine("Enter a number:");
+            ans = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1 + " " + n2 + " ");
+            for(i = 2; i < ans; i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+
+        }
+    }
+}
